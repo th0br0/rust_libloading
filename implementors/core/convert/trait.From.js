85fc590e95a788e4ea897567a209cf7711b78d23
Simonas Kazlauskas <git@kazlauskas.me>
2016-05-03 01:17:09 +0300

    Update docs to 0.2.2

--- implementors/core/convert/trait.From.js
+++ implementors/core/convert/trait.From.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libloading'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;NullError&gt; for <a class='struct' href='https://doc.rust-lang.org/nightly/std/io/error/struct.Error.html' title='std::io::error::Error'>Error</a>",];
+implementors['libloading'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='https://doc.rust-lang.org/nightly/std/net/ip/struct.Ipv4Addr.html' title='std::net::ip::Ipv4Addr'>Ipv4Addr</a>&gt; for <a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.u32.html'>u32</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;NullError&gt; for <a class='struct' href='https://doc.rust-lang.org/nightly/std/io/error/struct.Error.html' title='std::io::error::Error'>Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ implementors/core/ops/trait.BitAnd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libloading'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
