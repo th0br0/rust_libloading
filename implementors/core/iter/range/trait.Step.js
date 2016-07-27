c3fd9032dac65b5e7e223ab1df44340949b77f1a
Simonas Kazlauskas <git@kazlauskas.me>
2016-07-27 19:35:51 +0300

    Update docs for 0.3.0

--- implementors/core/iter/range/trait.Step.js
+++ implementors/core/iter/range/trait.Step.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libloading'] = [];
+implementors["lazy_static"] = [];implementors["libloading"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
